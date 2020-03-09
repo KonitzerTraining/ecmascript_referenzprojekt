4fe9308b16ae6ca487066cae46604b262c4a3693
KonitzerTraining <mk@konitzer-training.de>
2020-03-09 13:52:23 +0100

    More Comments

--- src/04_context/js/main.js
+++ src/04_context/js/main.js
@@ -1,17 +1,25 @@
+//"use strict";
+
 var color = 'green';
 
 console.log(color);
 console.log(window.color);
 console.log(this.color);
 
+/*
+Window ist das globale Object, this ist der globale Kontext
+ */
 // Bitte immer === anstatt == verwenden
 console.log(window === this);
 
 function sayColor(p) {
+    // this enthält den Kontext, der beim
+    // Aufruf con sayColor gilt
     console.log(p, this.color);
   //  this.project = p;
 }
 
+window.sayColor('global');
 sayColor('global');
 
 let contextA = {
@@ -22,7 +30,8 @@ sayColor.apply(contextA, ['mit contextA']);
 
 let contextB = {
     color: 'yellow',
-    sayColor: sayColor
+    //sayColor: sayColor
+    sayColor // ES6 Kurzschreibweise
 };
 
 contextB.sayColor('via ContextB');
@@ -36,17 +45,17 @@ let contextC = {
 
 contextC.sayColor('via ContextC');
 let kopie = contextC.sayColor;
-kopie('als Kopie');
+kopie('als Kopie'); // window.kopie();
 
 btn1.addEventListener('click', function () {
-  console.log(this);
+  console.dir(this);
 });
 
 //ES 3
-let _this = this;
+let _this = this; // window
 btn2.addEventListener('click', function () {
-    console.log(this);
-    console.log(_this);
+    console.log(this); // btn2
+    console.log(_this); // _this === window
 });
 
 //ES 5 IE 8 / 9
@@ -55,10 +64,13 @@ btn3.addEventListener('click', function (event) {
     console.log(this);
     console.log(event.target);
 
-}.bind(this));
+}.bind(this)); // this === window
+
 
 //ES 6 , ab Edge
 // Arrow Function
+// this wird bei der Definition der Arrow-Funktion
+// definiert
 btn4.addEventListener('click', (event) => {
     console.log(this);
     console.log(event.target);
@@ -72,7 +84,6 @@ let erg = fY(5);
 console.log(erg);
 
 let fY2 = x => x * x;
-
 let erg2 = fY2(5);
 console.log(erg);
 
@@ -88,6 +99,8 @@ new gibt Referenz zurück
 let p = new Person('Hans');
 console.log(p);
 
+// Exot: ES5
 let oX = Object.create(null, {});
 oX.color = 'green';
 console.log(oX);
+console.log(oX.constructor);
