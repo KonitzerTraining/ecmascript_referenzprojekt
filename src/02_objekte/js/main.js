dc9fd59321b3415aa967e9c347bbef10e04cbe78
KonitzerTraining <mk@konitzer-training.de>
2020-03-09 11:55:44 +0100

    More Comments

--- src/02_objekte/js/main.js
+++ src/02_objekte/js/main.js
@@ -1,33 +1,54 @@
 // ES3
-/*
-Object ist eine Funktion
- */
+
+// Einfaches Objekt
+// new ist ein Schlüsselwort zur Objekt-Erstellung
+// Object ist eine Funktion! Klassen gibt es in JS nicht
 let o = new Object();
 console.log(typeof Object);
+
+
+// Hinzufügen von Eigenschaften via .
 o.color = 'green';
+o['title'] = 'Headline';
 console.log(o);
 
-// let ist ES6
 // object literal notation
 let o1 = {
     color: 'yellow',
 };
-console.log(o1);
+console.log(o1, typeof o1);
+
+console.log(o1['color']);
+console.log(o1.color);
+
+// Fast jedes Object hat einen Prototyp
 console.log(o1.__proto__);
-console.log(typeof o1);
+console.log(Object.prototype === o1.__proto__);
 
-// ES 6
+
+// ES 6 - neue Schreibweise
 class Haus {
-
 }
-
 console.log(typeof Haus);
+let h = new Haus();
+console.log(h);
+
 
 // Alle Funktionen sind Objekte
 let f3 = new Function ('console.log("aha");');
 f3();
+console.log(typeof f3);
 
-function sayColor () {
+// Eigene Objekt
+function Person () {
+
 }
 
-console.dir(sayColor);
+let p = new Person();
+console.log(p);
+
+// Wrapper type
+let t1 = 'Text';
+console.log(typeof t1);
+console.log(t1.constructor);
+console.log(typeof String);
