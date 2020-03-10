ec1158c51cde494d5f3bf26010c8806db0a50a7a
KonitzerTraining <mk@konitzer-training.de>
2020-03-10 11:12:12 +0100

    More Comments

--- src/13_ES6_deconstructor/js/main.js
+++ src/13_ES6_deconstructor/js/main.js
@@ -1,7 +1,10 @@
-// Deconstructor
-let list = ['buch', '234.34234', 'J.R', '2', 'asdf'];
+let list = ['Herr der Ringe', '234.34234', 'J.R. Tolkien', 'Amazon Rank: 39893', '1937'];
 
-let [title, isbn, author = '', ...metadata] = list;
+// Deconstructor
+// Rest-Operator ... => Array
+// Default-Werte möglich
+let [title, isbn, author = null, ...metadata] = list;
+
 console.log(title);
 console.log(isbn);
 console.log(author);
@@ -13,6 +16,8 @@ let address = {
     city: 'Ilmenau',
 };
 
+// Default-Werte möglich
+// Alias mit :
 let {street, plz: zip, city, phone = null} = address;
 console.log(street);
 console.log(zip);
@@ -21,7 +26,7 @@ console.log(phone);
 
 setOrder(address);
 
-function setOrder({street, plz: zip} = null) {
+function setOrder({street = null, plz: zip = null}) {
     console.log(street)
 }
 
@@ -29,7 +34,8 @@ function setOrder({street, plz: zip} = null) {
 // Spread Operator ...
 
 let list1 = [3, 4, 5, 234, 45, 456, 243, 123, 345, 46];
-Math.max(...list1); // Spread
+let maxValue = Math.max(...list1); // Spread
+console.log(maxValue);
 
 compute(...list1);
 
@@ -38,4 +44,15 @@ function compute(a, b, ...c) {
     console.log(b);
     console.log(c);
 }
+
+let o1 = {
+    id: 3,
+    title: 'Überschrift',
+};
+
+let o2 = {
+    ...o1, // Spread auf Object möglich
+    author: 'me'
+};
+console.log(o2);
 
