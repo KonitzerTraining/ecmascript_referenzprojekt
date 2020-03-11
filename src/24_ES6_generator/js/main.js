777a49b9c1a6a31d46cc697c47b552720b7f0486
KonitzerTraining <mk@konitzer-training.de>
2020-03-11 11:35:25 +0100

    More Comments

--- src/24_ES6_generator/js/main.js
+++ src/24_ES6_generator/js/main.js
@@ -1,1 +1,22 @@
-console.log('ok');
+// Klassische Funktion
+function f1 ()  {
+    return 1;
+}
+let erg = f1();
+console.log(erg);
+
+// Generator-Funktion
+function* f2 () {
+  //  return 5;
+    let i = 0;
+    while (true) {
+        i++;
+        yield i;
+    }
+}
+
+let ergF2 = f2();
+console.log(ergF2.next());
+console.log(ergF2.next());
+console.log(ergF2.next());
+
