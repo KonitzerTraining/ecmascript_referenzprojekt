9ae7f447783f65cdc6d6cc8dfb09eae5bdcc4254
KonitzerTraining <mk@konitzer-training.de>
2020-03-11 09:59:21 +0100

    More Comments

--- src/20_ES6_map/js/main.js
+++ src/20_ES6_map/js/main.js
@@ -1,12 +1,22 @@
-//ES6
+//ES6 Collections
+// Array, Map, Set
 let map = new Map();
 
 console.log(Map.prototype);
-map.set({id: 3, title: 'Haus'}, 2);
+
+// map.set(key, value)
+let o1 = {id: 3, title: 'Haus'};
+map.set(o1, 2);
 map.set({id: 4, title: 'Baum'}, 1);
 map.set({id: 5, title: 'Moped'}, 7);
 console.log(map);
 
+// map = [34,54,234,43];
+for(let v of map) {
+    console.log(v);
+}
+
+// for (let [key, value] of map) {
 for (let [key, value] of map.entries()) {
     console.log(key, value);
 }
