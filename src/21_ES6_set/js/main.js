4f1edcc5fd9a556e75297738628d3eddd75eb4a9
KonitzerTraining <mk@konitzer-training.de>
2020-03-11 11:06:06 +0100

    More Comments

--- src/21_ES6_set/js/main.js
+++ src/21_ES6_set/js/main.js
@@ -1,9 +1,13 @@
+// ES6 Collection
+// Duplikate finden mit Set
+
 let items = [3,4,5,3,4,3,2,3];
 let list = new Set(items); // ab ES6
 console.dir(Set);
 
 console.log(list);
-console.log(Array.from(list));
+items = Array.from(list);
+console.log(items);
 
 let o1 = {a: 1};
 let o2 = {b: 2};
--- src/22_ES6_Symbol/js/main.js
+++ src/22_ES6_Symbol/js/main.js
@@ -1,1 +1,16 @@
-console.log('ok');
+// ES6 Datentyp Symbol
+
+let s1 = Symbol.for('populate');
+console.log(s1);
+
+let s2 = Symbol();
+console.log(s1 === s2);
+
+let s1a = Symbol.for('populate');
+console.log(s1 === s1a);
+
+let l1 = [1];
+console.log(l1);
+
+console.log(Symbol.match);
+console.dir(Symbol);
